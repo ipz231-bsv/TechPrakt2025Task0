4921bfa2786830da5bd8e1d4f829bf797be44b0e
((^199802^FZa^)) <ipz231_bsv@student.ztu.edu.ua>
2025-01-29 13:18:01 +0200

    Додано C# консольний додаток

---
+++ CSharpProject/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program {
+    static void Main() {
+        Console.Write("Введіть перше число: ");
+        int a = int.Parse(Console.ReadLine());
+        Console.Write("Введіть друге число: ");
+        int b = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Сума: {a + b}");
+    }
+}
